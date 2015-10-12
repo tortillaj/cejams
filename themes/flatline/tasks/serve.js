4281f850a3b6af902ae401a563c11911112d2145
James Cole <mail@ofjamescole.com>
2015-10-12 15:00:12 -0400

    update gulp tasks to process images during development

--- themes/flatline/tasks/serve.js
+++ themes/flatline/tasks/serve.js
@@ -13,10 +13,10 @@ gulp.task('serve', ['serve:setup'], function () {
 
   gulp.watch(['./layouts/**/*', '../../content/**/*', './archetypes/**/*', '../../config.toml', 'theme.toml']).on('change', function() { return sequence('hugo:draft', browserSync.reload) });
 
-  gulp.watch('assets/scss/**/*.scss', ['styles:dev']);
-  gulp.watch('assets/js/*.js').on('change', function() { return sequence('scripts:dev', browserSync.reload); });
-  gulp.watch('assets/img/*.*').on('change', function() { return sequence('images', browserSync.reload); });
-  gulp.watch('assets/svg/*.svg').on('change', function() { return sequence('svg', browserSync.reload); });
+  gulp.watch('assets/scss/**/*.scss', ['styles:dev', 'copy:styles:dev']);
+  gulp.watch('assets/js/*.js').on('change', function() { return sequence('scripts:dev', 'copy:scripts:dev', 'hugo:draft', browserSync.reload); });
+  gulp.watch('assets/img/*.*').on('change', function() { return sequence('images', 'copy:images:dev', 'hugo:draft', browserSync.reload); });
+  gulp.watch('assets/svg/*.svg').on('change', function() { return sequence('svg', 'copy:svg:dev', 'hugo:draft', browserSync.reload); });
   gulp.watch('assets/layouts/**/*.html').on('change', function() { return sequence('copy:layouts:dev', 'hugo:draft', browserSync.reload); });
 });
 
